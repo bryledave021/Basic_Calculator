38e5d2ea63e8bfb75d19d660768f83c2a2c964e8
Bryle Dave Martinez <72260896+bryledave021@users.noreply.github.com>
2021-06-30 12:59:13 +0800

    OOP-Basic Calculator
    Update: Converted into OOP method

---
+++ Methods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Basic_Calculator
+{
+    class Methods
+    {
+        private double FirstVariable;
+
+        public double FirstVariable1 { get => FirstVariable; set => FirstVariable = value; }
+        public double SecondVariable1 { get => SecondVariable; set => SecondVariable = value; }
+        public string Basic_Operations1 { get => Basic_Operations; set => Basic_Operations = value; }
+        public string AnswerCalc1 { get => AnswerCalc; set => AnswerCalc = value; }
+
+        private double SecondVariable;
+
+        private string Basic_Operations;
+        private string AnswerCalc;
+
+        public void Addition() {
+            AnswerCalc = (FirstVariable1 + SecondVariable1).ToString();
+            Basic_Operations1 = "Equals";
+
+        }
+        public void Subtraction() {
+            AnswerCalc = (FirstVariable1 - SecondVariable1).ToString();
+            Basic_Operations1 = "Equals";
+        }
+        public void Multiplication() {
+            AnswerCalc = (FirstVariable1 * SecondVariable1).ToString();
+            Basic_Operations1 = "Equals";
+        }
+        public void Division() {
+            AnswerCalc = (FirstVariable1 / SecondVariable1).ToString();
+            Basic_Operations1 = "Equals";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Basic_Calculator
+namespace OOP_Basic_Calculator
 {
     static class Program
     {
@@ -17,7 +17,7 @@ namespace Basic_Calculator
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MyCalculator());
         }
     }
 }
